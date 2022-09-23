a427db88e25ea314d57e5cdcec7a67ef2635611f
HawraaYounes <hawraayounes0@gmail.com>
2022-09-23 13:25:31 +0300

    Fixing jquery errors,placed jquery script before js script

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const getNews=()=>{
+    
     $.getJSON('http://localhost/newsjquery/news.php', function(data) {
         console.log(data);
       });
     }
-window.addEventListener ("load",getNews)//when page is loaded, getNews functionwill be executed
+$(document).ready(getNews);//when page is ready,execute getNews function
