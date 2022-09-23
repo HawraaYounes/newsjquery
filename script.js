3db441a172d9eb6aedebb50f6c4a7e516341fe13
HawraaYounes <hawraayounes0@gmail.com>
2022-09-23 13:35:56 +0300

    Looping over news api and getting its data in console for test

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+
 const getNews=()=>{
     
     $.getJSON('http://localhost/newsjquery/news.php', function(data) {
-        console.log(data);
+        $.each(data,function(key,value){
+            let id=value.id;
+            let news=value.new;
+            let date=value.date;
+            console.log(id);
+            console.log(date);
+            console.log(news);
+            //$("tbody").append("<tr>"+td+"</tr>")
+        })
       });
     }
 $(document).ready(getNews);//when page is ready,execute getNews function
