3f657af1094f58d401150cfec26b8df85d107bd3
HawraaYounes <hawraayounes0@gmail.com>
2022-09-23 13:51:19 +0300

    Commenting the code

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 
 const getNews=()=>{
     
-    $.getJSON('http://localhost/newsjquery/news.php', function(data) {
-        $.each(data,function(key,value){
-            let id="<th>"+value.id+"</th>";
-            let date="<td>"+value.date+"</td>";
-            let news="<td colspan='2'>"+value.new+"</td>";
-            let tr="<tr>"+id+date+news+"</tr>";
-            $("tbody").append(tr);
+    $.getJSON('http://localhost/newsjquery/news.php', function(data) {//fetch news api with getJSON
+        $.each(data,function(key,value){//looping over each object in api
+            let id="<th>"+value.id+"</th>";//create th tag for displaying row id from api
+            let date="<td>"+value.date+"</td>";//create td tag for displaying new date from api
+            let news="<td colspan='2'>"+value.new+"</td>";//create td tag for displaying new text from api
+            let tr="<tr>"+id+date+news+"</tr>";//create tr tag containg the 3 previous td tags
+            $("tbody").append(tr);//append created tr to table body
         })
       });
     }
